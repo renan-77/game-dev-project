bd25deafab623546564b02263021c4a884a73cb8
Chris O Shea <christoper.oshea@mycit.ie>
2021-12-15 17:12:46 +0000

    added movement/animation

--- The Return of The Devil/game/jeremiah.go
+++ The Return of The Devil/game/jeremiah.go
@@ -1,3 +1,18 @@
+components {
+  id: "player"
+  component: "/game/player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -22,8 +37,8 @@ embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 10.0\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
   "group: \"player\"\n"
