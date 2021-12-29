a5d32fc1554ec998ee8b4f4820b0462f727b7f61
Chris O Shea <christoper.oshea@mycit.ie>
2021-12-29 16:37:50 +0000

    added respawn

--- The Return of The Devil/game/jeremiah.go
+++ The Return of The Devil/game/jeremiah.go
@@ -44,6 +44,7 @@ embedded_components {
   "group: \"player\"\n"
   "mask: \"ground\"\n"
   "mask: \"platform\"\n"
+  "mask: \"restart\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
